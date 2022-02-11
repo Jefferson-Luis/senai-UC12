ca0bfd6051b851c2bd9347987f21ab9d3334ab1a
Jefferson-Luis <jeffersonalveti@gmail.com>
2022-02-10 23:56:25 -0300

    terceiro commit

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import { VagasService } from '../vagas.service';
+import { Vaga } from '../models/Vagas.model';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  public vaga: Vaga = new Vaga(0,"","","",0);
+  public vagas: Vaga[] = [];
 
-  constructor() { }
+  constructor(private _vagasService: VagasService) { }
 
   ngOnInit(): void {
+    this.listarVagas();
+  }
+  cadastrar(){
+    this._vagasService.cadastrarVaga(this.vaga).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log("erro ao cadastrar")}
+    );
+
+    window.location.href = "/mural";
+
   }
 
+  atualizar(id: number){
+    this._vagasService.atualizarVaga(id,this.vaga).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log("erro ao atualizar")}
+    );
+
+    window.location.href = "/mural";
+
+  }
+
+  excluir(id: number){
+    this._vagasService.removerVaga(id).subscribe(
+      vaga => {this.vaga = new Vaga(0,"","","",0)},
+      err => {console.log("erro ao Excluir")}
+    );
+
+    window.location.href = "/mural";
+
+  }
+  
+  listarVagas(){
+    this._vagasService.getVagas()
+      .subscribe(
+        retornaVaga => {
+          this.vagas = retornaVaga.map(
+            item=>{
+              return new Vaga(
+                item.id,
+                item.nome,
+                item.foto,
+                item.descricao,
+                item.salario
+              );
+            }
+          )
+        }
+      )
+  }
 }
